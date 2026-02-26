f637c6dd9b49fe86a83f5104e54d0961f2e83208
gartno-jpg <oaranyeves@alumnes.ilerna.com>
2026-02-26 17:19:09 +0100

    Creado el main de JocPengui
    Creado el main de JocPengui

---
+++ Proyecto/ProyectoJava/src/JocPengui/Main.java
@@ -0,0 +1,17 @@
+package JocPengui;
+
+
+public class Main {
+	
+	public main() {
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al Juego: ");
+		
+		
+		PantallaMenu menu = new PantallaMenu();
+		menu.mostrarMenu();
+	}
+
+}
