cee5125d703a2aa0498b9e59f3e78e2f599ef583
sk0luu <cxasx2006@alumnes.ilerna.com>
2026-02-26 16:04:39 +0100

    Clase Dado terminada
    Clase Dado terminada

--- ProjectoJava/src/JocPengui/Dado.java
+++ ProjectoJava/src/JocPengui/Dado.java
@@ -1,5 +1,28 @@
 package JocPengui;
-
+import java.util.Random;
 public class Dado {
-
+	private int max;
+	private int min;
+	public Dado(int max, int min) {
+		this.max = max;
+		this.min = min;
+	}
+	public int getMax() {
+		return max;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+	public int getMin() {
+		return min;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
+	public int tirar() {
+		Random r = new Random();
+		int numeroRandom = r.nextInt(min) + max;
+		return numeroRandom;
+		
+	}
 }
