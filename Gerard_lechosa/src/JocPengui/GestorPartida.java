80da4dafbcf163621a33d0fcc5176c0e374a0695
gerardlechosa-cmyk <gerardlechosa@alumnes.ilerna.com>
2026-02-27 19:44:00 +0100

    feat(gestor-partida):
     implementar nuevaPartida(ArrayList<Jugador>, Tablero)

--- Gerard_lechosa/src/JocPengui/GestorPartida.java
+++ Gerard_lechosa/src/JocPengui/GestorPartida.java
@@ -1,42 +1,28 @@
-package JocPengui;
-
-import java.util.Random;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GestorPartida {
-
     private Partida partida;
     private GestorTablero gestorTablero;
     private GestorJugador gestorJugador;
+    private BBDD bbdd;
     private Random random;
 
-    public GestorPartida() {
+    public GestorPartida(GestorTablero gestorTablero, GestorJugador gestorJugador, BBDD bbdd) {
+        this.gestorTablero = gestorTablero;
+        this.gestorJugador = gestorJugador;
+        this.bbdd = bbdd;
         this.random = new Random();
     }
 
-    // crea una partida nueva segun UML
     public void nuevaPartida(ArrayList<Jugador> jugadores, Tablero tablero) {
-        // crea partida
-        partida = new Partida();
-
-        // anade jugadores
-        for (Jugador j : jugadores) {
-            partida.afegirJugador(j);
-        }
-
-        // asigna tablero
+        this.partida = new Partida();
+        partida.setJugadores(jugadores);
         partida.setTablero(tablero);
-
-        // crea controladores
-        gestorTablero = new GestorTablero(tablero);
-        gestorJugador = new GestorJugador();
-
-        // anade jugadores a gestorJugador
-        for (Jugador j : jugadores) {
-            gestorJugador.afegirJugador(j);
-        }
-
-        // inicia turno
-        partida.setTornActual(0);
+        partida.setTurnos(0);
+        partida.setJugadorActual(0);
+        partida.setFinalizada(false);
+        partida.setGanador(null);
+        actualizarEstadoTablero();
     }
 }
