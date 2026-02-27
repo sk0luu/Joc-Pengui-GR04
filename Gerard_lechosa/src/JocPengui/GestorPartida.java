82dc5a06c84e95b46d208325cb7f951f5efe5e5d
gerardlechosa-cmyk <gerardlechosa@alumnes.ilerna.com>
2026-02-27 17:14:09 +0100

    feat(gestorpartida):
    implementar nuevaPartida

--- Gerard_lechosa/src/JocPengui/GestorPartida.java
+++ Gerard_lechosa/src/JocPengui/GestorPartida.java
@@ -5,15 +5,38 @@ import java.util.ArrayList;
 
 public class GestorPartida {
 
-    // atributos segun el diagrama
     private Partida partida;
     private GestorTablero gestorTablero;
     private GestorJugador gestorJugador;
     private Random random;
 
-    // constructor vacio
     public GestorPartida() {
-        // inicializa random
         this.random = new Random();
+    }
+
+    // crea una partida nueva segun UML
+    public void nuevaPartida(ArrayList<Jugador> jugadores, Tablero tablero) {
+        // crea partida
+        partida = new Partida();
+
+        // anade jugadores
+        for (Jugador j : jugadores) {
+            partida.afegirJugador(j);
+        }
+
+        // asigna tablero
+        partida.setTablero(tablero);
+
+        // crea controladores
+        gestorTablero = new GestorTablero(tablero);
+        gestorJugador = new GestorJugador();
+
+        // anade jugadores a gestorJugador
+        for (Jugador j : jugadores) {
+            gestorJugador.afegirJugador(j);
+        }
+
+        // inicia turno
+        partida.setTornActual(0);
     }
 }
