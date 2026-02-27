554e5f5626108277101a90efd2aa77f802b411c2
gerardlechosa-cmyk <gerardlechosa@alumnes.ilerna.com>
2026-02-27 17:01:59 +0100

    feat(gestorpartida):
    crear estructura inicial de GestorPartida segun UML

--- Gerard_lechosa/src/JocPengui/GestorPartida.java
+++ Gerard_lechosa/src/JocPengui/GestorPartida.java
@@ -1,5 +1,19 @@
 package JocPengui;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 public class GestorPartida {
 
+    // atributos segun el diagrama
+    private Partida partida;
+    private GestorTablero gestorTablero;
+    private GestorJugador gestorJugador;
+    private Random random;
+
+    // constructor vacio
+    public GestorPartida() {
+        // inicializa random
+        this.random = new Random();
+    }
 }
