e530ec0bafc75cdc6879994cb67eb99c5d22d753
sk0luu <cxasx2006@alumnes.ilerna.com>
2026-03-04 16:52:59 +0100

    Errores resueltos

--- ProjectoJava/src/JocPengui/Dado.java
+++ ProjectoJava/src/JocPengui/Dado.java
@@ -1,28 +1,10 @@
 package JocPengui;
 import java.util.Random;
 public class Dado {
-	private int max;
-	private int min;
-	public Dado(int max, int min) {
-		this.max = max;
-		this.min = min;
-	}
-	public int getMax() {
-		return max;
-	}
-	public void setMax(int max) {
-		this.max = max;
-	}
-	public int getMin() {
-		return min;
-	}
-	public void setMin(int min) {
-		this.min = min;
-	}
-	
+	Random r = new Random();
 	public int tirar() {
-		Random r = new Random();
-		int random = r.nextInt(1) + 7;
+		
+		int random = r.nextInt(6) + 1;
 		return random;
 	}
 }
